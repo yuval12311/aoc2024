93d674f03aa7c95679c55f89ee25b3d9dc9ea00d
yuval12311 <yuval.nosov@gmail.com>
2024-12-02 21:29:07 +0200

    Finished day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Day2;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine(SolveFile("C:\\Users\\yuval\\RiderProjects\\AdventOfCode2024\\Day2\\input.txt", IsSafeLevel));
+        Console.WriteLine(SolveFile("C:\\Users\\yuval\\RiderProjects\\AdventOfCode2024\\Day2\\input.txt", IsSafeLevel2));
+    }
+
+    private static int SolveFile(string inputPath, Func<List<int>, bool> condition)
+    {
+        return File.ReadAllLines(inputPath)
+            .Select(l => l.Split(" ").Select(int.Parse).ToList())
+            .Where(condition)
+            .Count();
+    }
+
+    private static bool IsSafeLevel(List<int> level)
+    {
+        int isIncreasing = level[0] < level[1] ? 1 : -1;
+        for (int i = 0; i < level.Count - 1; i++)
+        {
+             var diff = isIncreasing * (level[i + 1] - level[i]);
+             if (diff is <= 0 or > 3)
+             {
+                 //Console.WriteLine($"level [{string.Join(", ", level)}] bad: {level[i]}, {level[i + 1]}");
+                 return false;
+             }
+        }
+        return true;
+    }
+    
+    private static bool IsSafeLevel2(List<int> level)
+    {
+        return IsSafeLevel(level) || Enumerable.Range(0, level.Count).Any(i =>
+        {
+            var copy = level.ToList();
+            copy.RemoveAt(i);
+            return IsSafeLevel(copy);
+        });
+    }
+
+    
+
+    private static bool DiffBad(int[] level, int isIncreasing, int low, int high)
+    {
+        var diff = isIncreasing * (level[high] - level[low]);
+        return diff is <= 0 or > 3;
+    }
+}
