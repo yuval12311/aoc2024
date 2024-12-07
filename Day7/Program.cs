d403ccf62b0105fa9a5529476f856b376f753ec0
yuval12311 <yuval.nosov@gmail.com>
2024-12-07 12:42:12 +0200

    Completed day 7 easy win

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,9 +1,76 @@
-﻿namespace Day7;
+﻿
+namespace Day7;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var t = new TimeOnly();
+
+        Console.WriteLine(SolveFile("..\\..\\..\\\\input.txt"));
+
+        Console.WriteLine((new TimeOnly() - t).TotalNanoseconds);
+    }
+
+    private static long SolveFile(string inputTxt)
+    {
+        return File
+            .ReadLines(inputTxt
+            )
+            .Select(line =>
+            {
+                var split = line.Split(": ");
+                return (long.Parse(split[0]), split[1].Split(' '));
+            })
+            .Where(pair => SolveEquation(
+                pair.Item2.Select(int.Parse).ToArray(),
+                pair.Item1))
+            .Sum(pair =>pair.Item1);
+    }
+    
+
+    public static bool SolveEquation(Span<int> input, long result)
+    {
+        return SolveEquationRec(input, result);
+    }
+    
+    private static bool SolveEquationRec(Span<int> input, long result)
+    {
+        if (input.Length == 1)
+        {
+            return input[0] == result;
+        }
+        if (result % input[^1] == 0 && SolveEquationRec(input[..^1], result / input[^1]))
+        {
+            return true;
+        }
+
+        if (EndsWith(result, input[^1]) && SolveEquationRec(input[..^1], RemoveDigits(result, input[^1])))
+        {
+            return true;
+        }
+
+        return SolveEquationRec(input[..^1], result - input[^1]);
+    }
+
+    private static long RemoveDigits(long result , int i)
+    {
+        while (i > 0)
+        {
+            i /= 10;
+            result /= 10;
+        }
+        return result;
+    }
+
+    private static bool EndsWith(long num1, int num2)
+    {
+        while (num1 > 0 && num2 > 0)
+        {
+            if (num2 % 10 != (num1 % 10) ) return false;
+            num2 /= 10;
+            num1 /= 10;
+        }
+        return true;
     }
 }
