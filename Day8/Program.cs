4be4a5977394ea10ac093aabe889d537fb633a5b
Yuval Nosovitsky <yuval.nosov@gmail.com>
2024-12-08 18:41:11 +0200

    Added day 8

---
+++ Day8/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Day8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var t = new TimeOnly();
+
+        Console.WriteLine(SolveFile("..\\..\\..\\\\input.txt"));
+
+        Console.WriteLine((new TimeOnly() - t).TotalNanoseconds);
+    }
+
+    private static int SolveFile(string inputFile)
+    {
+        return 0;
+    }
+}
