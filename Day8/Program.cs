8658af05e21521643ce85b35dd65c0802f5ed679
Yuval Nosovitsky <yuval.nosov@gmail.com>
2024-12-08 19:32:49 +0200

    Finished part 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Numerics;
+using System.Text;
 
 namespace Day8;
 
@@ -20,6 +21,7 @@ class Program
     {
         public static Vector2<T> operator +(Vector2<T> a, Vector2<T> b) => new(a.x + b.x, a.y + b.y);
         public static Vector2<T> operator -(Vector2<T> a, Vector2<T> b) => new(a.x - b.x, a.y - b.y);
+        public static Vector2<T> operator *(T a, Vector2<T> b) => new(a *  b.x, a * b.y);
 
     }
 
@@ -28,20 +30,42 @@ class Program
         var text = File.ReadAllLines(inputFile);
         int maxI = text.Length - 1;
         int maxJ = text[0].Length - 1;
-
-        return text
+        var vecs = text
             .SelectMany((line, i) => line.Select((c, j) => new { Char = c, Point = new Vector2<int>(i, j) }))
             .Where(t => t.Char != '.')
             .GroupBy(t => t.Char)
             .SelectMany(g =>
-                g.SelectMany((p, i) => g.Skip(i + 1),
+                g.SelectMany((p, i) => g,
                         (p1, p2) => (p1.Point, p2.Point))
-                    .SelectMany(pair => new List<Vector2<int>>
-                        { pair.Item1 + pair.Item1 - pair.Item2, pair.Item2 + pair.Item2 - pair.Item1 })
-                    .Where(p => IsInBounds(p, maxI, maxJ))
+                    .Where(pair => pair.Item1 != pair.Item2)
+                    .SelectMany(pair => Enumerable.Range(0, Math.Max(maxI, maxJ))
+                        .Select(i => pair.Item1 + i * (pair.Item1 - pair.Item2))
+                        //.Take(1)
+                        .TakeWhile(p => IsInBounds(p, maxI, maxJ))
+                    )
             )
             .Distinct()
+            .ToList();
+
+        PrintAnti(text, vecs);
+
+        return vecs
             .Count();
+    }
+
+    private static void PrintAnti(string[] text, List<Vector2<int>> vecs)
+    {
+        var chars = text.Select(s => s.ToCharArray()).ToArray();
+        foreach (var vec in vecs)
+        {
+            chars[vec.x][vec.y] = '#';
+        }
+
+        foreach (var str in chars)
+        {
+            Console.WriteLine(str);
+        }
+        
     }
 
     private static bool IsInBounds(Vector2<int> vector, int maxI, int maxJ)
