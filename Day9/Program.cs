c4ee7d62d0489537217c816a50434603ad73164c
Yuval Nosovitsky <yuval.nosov@gmail.com>
2024-12-10 00:26:15 +0200

    finished part 1

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -6,25 +6,28 @@ class Program
     {
         var t = new TimeOnly();
         
-        Console.WriteLine(SolveFile("..\\..\\..\\\\test.txt"));
+        Console.WriteLine(SolveFile("..\\..\\..\\\\input.txt"));
 
         Console.WriteLine((new TimeOnly() - t).TotalNanoseconds);
     }
 
-    private static int SolveFile(string inputFile)
+    private static long SolveFile(string inputFile)
     {
         var str = File.ReadAllText(inputFile);
         List<(int id, int lenght)> originalBlocks = str.Select((c, i) => (i % 2 == 0 ? i / 2 : -1, c - '0')).ToList();
         List<(int id, int length)> newBlocks = new List<(int, int)>();
-        int lastBloc = originalBlocks.Count;
+        int lastBloc = originalBlocks.Count - 1;
         int currentBloc = 0;
         int currentId = 0;
-        while (currentBloc < lastBloc)
+        while (currentBloc <= lastBloc)
         {
             if (originalBlocks[currentBloc].id != -1)
             {
                 newBlocks.Add(originalBlocks[currentBloc]);
                 currentBloc++;
+            } else if (originalBlocks[lastBloc].id == -1)
+            {
+                lastBloc--;
             }
             else
             {
@@ -51,8 +54,9 @@ class Program
             }
         }
         
-        return newBlocks.Aggregate(new {Index=0, Sum = 0}, (acc, block) => 
-            new {Index = acc.Index + block.length, Sum = acc.Sum + block.id * (acc.Index * block.length + block.length * (block.id + 1) / 2)}).Sum;
+        //Console.WriteLine(string.Join(", ", newBlocks));
+        return newBlocks.Aggregate(new {Index=0L, Sum = 0L}, (acc, block) => 
+            new {Index = acc.Index + block.length, Sum = acc.Sum + block.id * (acc.Index * block.length + block.length * (block.length - 1) / 2)}).Sum;
          
     }
 }
