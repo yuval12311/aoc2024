443f3ba8f80bb4f083450440821aa31633618196
Yuval Nosovitsky <yuval.nosov@gmail.com>
2024-12-12 19:27:12 +0200

    Add Day12

---
+++ Day12/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var t = new TimeOnly();
+        
+        Console.WriteLine(SolveFile("..\\..\\..\\\\test.txt"));
+        //Console.WriteLine(SolveFile2("..\\..\\..\\\\input.txt"));
+
+        Console.WriteLine((new TimeOnly() - t).TotalNanoseconds);
+    }
+
+    private static bool SolveFile(string testTxt)
+    {
+        throw new NotImplementedException();
+    }
+}
