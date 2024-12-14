0218eb66db0086e58a002310855c95860be5bc44
Yuval Nosovitsky <yuval.nosov@gmail.com>
2024-12-14 10:13:17 +0200

    Started day 14

---
+++ Day14/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var t = new TimeOnly();
+        
+        Console.WriteLine(SolveFile("..\\..\\..\\\\input.txt"));
+
+        Console.WriteLine((new TimeOnly() - t).TotalNanoseconds);
+    }
+
+    private static int SolveFile(string inputFile)
+    {
+        throw new NotImplementedException();
+    }
+}
