a5b948d581df0cad95a493a7fba88d4e6a0a45e6
yuval12311 <yuval.nosov@gmail.com>
2024-12-15 20:54:23 +0200

    Added day 15

---
+++ Day15/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day15;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var t = new TimeOnly();
+
+        //Console.WriteLine(SolveFile("..\\..\\..\\\\input.txt"));
+        SolveFile("..\\..\\..\\\\input.txt");
+        Console.WriteLine((new TimeOnly() - t).TotalNanoseconds);
+    }
+
+    private static int SolveFile(string inputFile)
+    {
+        throw new NotImplementedException();
+    }
+}
