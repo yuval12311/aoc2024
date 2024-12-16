3c875f424de687d0ba885a66467ed874180f3124
yuval12311 <yuval.nosov@gmail.com>
2024-12-17 00:02:43 +0200

    Finished part 2

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 
 namespace Day16;
 
@@ -17,7 +18,7 @@ class Program
     private static int SolveFile(string inputFile)
     {
         var lines = File.ReadAllLines(inputFile);
-        (Dictionary<PointAndDir, int> costs, Dictionary<PointAndDir, PointAndDir> parents) = ParseInput(lines);
+        (Dictionary<PointAndDir, int> costs, Dictionary<PointAndDir, List<PointAndDir>> parents) = ParseInput(lines);
 
         Dictionary<PointAndDir, int> Graph(PointAndDir pointAndDir)
         {
@@ -33,26 +34,74 @@ class Program
         }
 
         DijkstraAlgorithm.FindShortestPath(Graph, costs, parents);
+        HashSet<(int i, int j)> onBestTrails =  GetBestTrails(parents, 
+            Enumerable.Range(0, 4)
+                .Select(dir => new PointAndDir { I= 1, J = lines[0].Length -2, Dir = dir })
+                .MinBy(pointAndDir => costs[pointAndDir])
+            );
+        //PrintTrails(lines, onBestTrails);
+        Console.WriteLine(onBestTrails.Count);
         return Enumerable.Range(0, 4).Select(dir => costs[new PointAndDir(1, lines.Length - 2, dir)]).Min();
     }
-    
+
+    private static void PrintTrails(string[] lines, HashSet<(int i, int j)> onBestTrails)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < lines[0].Length; j++)
+            {
+                if (onBestTrails.Contains((i, j)))
+                {
+                    sb.Append('O');
+                }
+                else
+                {
+                    sb.Append(lines[i][j]);
+                }
+            }
+            sb.AppendLine();
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+
+    private static HashSet<(int i, int j)> GetBestTrails(Dictionary<PointAndDir,List<PointAndDir>> parents, PointAndDir startingPoint)
+    {
+        HashSet<(int i, int j)> path = [];
+        Queue<PointAndDir> queue = new();
+        
+        queue.Enqueue(startingPoint);
+        while (queue.Count > 0)
+        {
+            var pointAndDir = queue.Dequeue();
+            path.Add((pointAndDir.I, pointAndDir.J));
+            foreach (var parent in parents[pointAndDir])
+            {
+                queue.Enqueue(parent);
+            }
+        }
+        return path;
+    }
+
 
     public static int MoveInIDir(int dir) => dir switch { 1 => -1, 3 => 1, _ => 0 };
     public static int MoveInJDir(int dir) => dir switch { 0 => 1, 2 => -1, _ => 0 };
 
     
-    private static (Dictionary<PointAndDir, int> costs, Dictionary<PointAndDir, PointAndDir> parents) ParseInput(string[] lines)
+    private static (Dictionary<PointAndDir, int> costs, Dictionary<PointAndDir, List<PointAndDir>> parents) ParseInput(string[] lines)
     {
         var costs = new Dictionary<PointAndDir, int>();
-        var parents = new Dictionary<PointAndDir, PointAndDir>();
+        var parents = new Dictionary<PointAndDir, List<PointAndDir>>();
         for (int i = 0; i < lines.Length; i++)
         {
             for (int j = 0; j < lines[0].Length; j++)
             {
                 if (lines[i][j] == 'S')
                 {
-                    parents.Add(new PointAndDir(i, j, 1), new PointAndDir(i, j, 0));
-                    parents.Add(new PointAndDir(i, j, 3), new PointAndDir(i, j, 0));
+                    parents.Add(new PointAndDir(i, j, 1), [new PointAndDir(i, j, 0)]);
+                    parents.Add(new PointAndDir(i, j, 0), []);
+                    parents.Add(new PointAndDir(i, j, 3), [new PointAndDir(i, j, 0)]);
                     costs.Add(new PointAndDir(i, j, 1), 1000);
                     costs.Add(new PointAndDir(i, j, 0), 0);
                     costs.Add(new PointAndDir(i, j, 3), 1000);
@@ -63,7 +112,7 @@ class Program
                 {
                     if (lines[i][j] == '.' && lines[i][j-1] == 'S' && dir == 0)
                     {
-                        parents.Add(new PointAndDir(i, j, 0), new PointAndDir(i, j-1, 0));
+                        parents.Add(new PointAndDir(i, j, 0), [new PointAndDir(i, j-1, 0)]);
                         costs.Add(new PointAndDir(i, j, 0), 1);
                     }
                     else if (lines[i][j] == '.' || lines[i][j] == 'E')
@@ -76,19 +125,19 @@ class Program
         return (costs, parents);
     }
 
-    public record  PointAndDir(int I, int J, int Dir);
+    public record struct PointAndDir(int I, int J, int Dir);
     
     public static class DijkstraAlgorithm
     {
         public static void FindShortestPath<T>(Func<T, Dictionary<T, int>> graph,
             Dictionary<T, int> costs,
-            Dictionary<T, T> parents) 
+            Dictionary<T, List<T>> parents) 
         {
             // Store the visited nodes
             var visited = new HashSet<T>();
 
-            var lowestCostUnvisitedNode = FindLowestCostUnvisitedNode(costs, visited);
-            while (lowestCostUnvisitedNode != null)
+            var isDone = FindLowestCostUnvisitedNode(costs, visited, out var lowestCostUnvisitedNode);
+            while (!isDone)
             {
                 var cost = costs[lowestCostUnvisitedNode];
                 var neighbors = graph(lowestCostUnvisitedNode);
@@ -100,25 +149,32 @@ class Program
                     if (newCost < costs[neighbor.Key])
                     {
                         costs[neighbor.Key] = newCost;
-                        parents[neighbor.Key] = lowestCostUnvisitedNode;
+                        parents[neighbor.Key] = [lowestCostUnvisitedNode];
+                    }
+                    else if (newCost == costs[neighbor.Key])
+                    {
+                        parents[neighbor.Key].Add(lowestCostUnvisitedNode);
                     }
                 }
 
                 visited.Add(lowestCostUnvisitedNode);
-                lowestCostUnvisitedNode = FindLowestCostUnvisitedNode(costs, visited);
-                Console.SetCursorPosition(0, 0);
-                if (visited.Count %100 == 0)Console.WriteLine(visited.Count + "/" +costs.Count );
+                isDone = FindLowestCostUnvisitedNode(costs, visited, out lowestCostUnvisitedNode);
+                if (visited.Count % 100 == 0)
+                {
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine(visited.Count + "/" +costs.Count );
+                }
             }
         }
 
-        private static T? FindLowestCostUnvisitedNode<T>(Dictionary<T, int> costs, HashSet<T> visited)
+        private static bool FindLowestCostUnvisitedNode<T>(Dictionary<T, int> costs, HashSet<T> visited, out T lowestCostUnvisitedNode)
         {
-            return costs
+            var unvisitedNode = costs
                 .Where(node => !visited.Contains(node.Key))
                 .OrderBy(node => node.Value)
-                // When .Where above returns empty collection, this will give default KeyValuePair: { Key = null, Value = 0 }
-                .FirstOrDefault()
-                .Key;
+                .ToList();
+            lowestCostUnvisitedNode = unvisitedNode.FirstOrDefault().Key;
+            return unvisitedNode.Count == 0;
         }
     }
 }
