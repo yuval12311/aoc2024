0cc393aafa561bec79583b56969dbcc56fa9ac98
yuval12311 <yuval.nosov@gmail.com>
2024-12-16 23:29:37 +0200

    Part 1 done

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -9,24 +9,24 @@ class Program
         var t = new TimeOnly();
 
         //Console.WriteLine(SolveFile("..\\..\\..\\\\input.txt"));
-        Console.WriteLine(SolveFile("..\\..\\..\\\\test.txt"));
+        Console.WriteLine(SolveFile("..\\..\\..\\\\input.txt"));
         Console.WriteLine((new TimeOnly() - t).TotalNanoseconds);
     }
 
 
     private static int SolveFile(string inputFile)
-    {\
+    {
         var lines = File.ReadAllLines(inputFile);
         (Dictionary<PointAndDir, int> costs, Dictionary<PointAndDir, PointAndDir> parents) = ParseInput(lines);
 
         Dictionary<PointAndDir, int> Graph(PointAndDir pointAndDir)
         {
-            var dictionary = new Dictionary<PointAndDir, int> { [pointAndDir with { Dir = (pointAndDir.Dir - 1) % 4 }] = 1000, [pointAndDir with { Dir = (pointAndDir.Dir + 1) % 4 }] = 1000, };
+            var dictionary = new Dictionary<PointAndDir, int> { [pointAndDir with { Dir = (pointAndDir.Dir - 1 + 4) % 4 }] = 1000, [pointAndDir with { Dir = (pointAndDir.Dir + 1 + 4) % 4 }] = 1000, };
             var nextI = pointAndDir.I + MoveInIDir(pointAndDir.Dir);
             var nextJ = pointAndDir.J + MoveInJDir(pointAndDir.Dir);
-            if (lines[nextI][nextJ] == '.')
+            if (lines[nextI][nextJ] == '.' || lines[nextI][nextJ] == 'E')
             {
-                dictionary[pointAndDir with { I = nextI, J = nextJ }] = 0;
+                dictionary[pointAndDir with { I = nextI, J = nextJ }] = 1;
             }
 
             return dictionary;
@@ -36,7 +36,6 @@ class Program
         return Enumerable.Range(0, 4).Select(dir => costs[new PointAndDir(1, lines.Length - 2, dir)]).Min();
     }
     
-    public static int Test<T>(List<T?> list) => 0;
 
     public static int MoveInIDir(int dir) => dir switch { 1 => -1, 3 => 1, _ => 0 };
     public static int MoveInJDir(int dir) => dir switch { 0 => 1, 2 => -1, _ => 0 };
@@ -45,7 +44,7 @@ class Program
     private static (Dictionary<PointAndDir, int> costs, Dictionary<PointAndDir, PointAndDir> parents) ParseInput(string[] lines)
     {
         var costs = new Dictionary<PointAndDir, int>();
-        var parents = new Dictionary<PointAndDir, PointAndDir?>();
+        var parents = new Dictionary<PointAndDir, PointAndDir>();
         for (int i = 0; i < lines.Length; i++)
         {
             for (int j = 0; j < lines[0].Length; j++)
@@ -55,7 +54,9 @@ class Program
                     parents.Add(new PointAndDir(i, j, 1), new PointAndDir(i, j, 0));
                     parents.Add(new PointAndDir(i, j, 3), new PointAndDir(i, j, 0));
                     costs.Add(new PointAndDir(i, j, 1), 1000);
+                    costs.Add(new PointAndDir(i, j, 0), 0);
                     costs.Add(new PointAndDir(i, j, 3), 1000);
+                    costs.Add(new PointAndDir(i, j, 2), int.MaxValue);
                     continue;
                 }
                 for (int dir = 0; dir < 4; dir++)
@@ -67,7 +68,6 @@ class Program
                     }
                     else if (lines[i][j] == '.' || lines[i][j] == 'E')
                     {
-                        parents.Add(new PointAndDir(i, j, dir), null);
                         costs.Add(new PointAndDir(i, j, dir), int.MaxValue);
                     }
                 }
@@ -76,13 +76,13 @@ class Program
         return (costs, parents);
     }
 
-    public record struct PointAndDir(int I, int J, int Dir);
+    public record  PointAndDir(int I, int J, int Dir);
     
     public static class DijkstraAlgorithm
     {
         public static void FindShortestPath<T>(Func<T, Dictionary<T, int>> graph,
             Dictionary<T, int> costs,
-            Dictionary<T, T?> parents) 
+            Dictionary<T, T> parents) 
         {
             // Store the visited nodes
             var visited = new HashSet<T>();
@@ -106,6 +106,8 @@ class Program
 
                 visited.Add(lowestCostUnvisitedNode);
                 lowestCostUnvisitedNode = FindLowestCostUnvisitedNode(costs, visited);
+                Console.SetCursorPosition(0, 0);
+                if (visited.Count %100 == 0)Console.WriteLine(visited.Count + "/" +costs.Count );
             }
         }
 
